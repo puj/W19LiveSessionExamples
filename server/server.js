d4a4623c90c23a1f3ba77d0b8ec0447e34ae09b0
Van <vango85@gmail.com>
2020-05-17 23:03:53 +0200

    [xx] Updated to include a likes:100 test for review

--- server/server.js
+++ server/server.js
@@ -1,124 +1,128 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-import booksData from './data/books.json'
+import booksData from "./data/books.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = Promise
+const ERR_CANNOT_FIND_ISBN = "Cannot find book";
 
-const Book = mongoose.model('Book', {
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/WK19PostRequests";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const Book = mongoose.model("Book", {
   bookID: {
-    type: Number
+    type: Number,
   },
   title: {
-    type: String
+    type: String,
   },
   authors: {
-    type: String
+    type: String,
   },
   average_rating: {
-    type: Number
+    type: Number,
   },
   isbn: {
     unique: true,
-    type: String
+    type: String,
   },
   isbn13: {
-    type: String
+    type: String,
   },
   language_code: {
-    type: String
+    type: String,
   },
   num_pages: {
-    type: Number
+    type: Number,
   },
   ratings_count: {
-    type: Number
+    type: Number,
   },
   text_reviews_count: {
-    type: Number
+    type: Number,
   },
   read: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 
-// booksData.forEach(book=> {
-//     new Book(book).save();
-// });
+const Review = mongoose.model("Review", {
+  review: {
+    type: String,
+  },
+  book: {
+    type: { type: mongoose.Schema.Types.ObjectId, ref: "Book" },
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
 
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Book.deleteMany();
+    await Review.deleteMany();
 
-const port = process.env.PORT || 8080
-const app = express()
+    await booksData.forEach((book) => {
+      new Book(book).save();
+    });
+  };
+  seedDatabase();
+}
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/books/:isbn/review', async (req, res) => {
-  const {isbn} = req.params;
+app.post("/books/:isbn/review", async (req, res) => {
+  const { isbn } = req.params;
+  const review = req.body;
+
   console.log(`POST /books/${isbn}/review`);
-  await Book.updateOne({'isbn': isbn}, {'$inc': {'text_reviews_count': 1}});
+  await Book.updateOne({ isbn: isbn }, { $inc: { text_reviews_count: 1 } });
+  await new Review({ review: review.review, book: review.book }).save();
+  // await new Review(review).save();
   res.status(201).json();
 });
 
-app.put('/books/:isbn/read', async (req, res) => {
-  const {isbn} = req.params;
+app.put("/books/:isbn/read", async (req, res) => {
+  const { isbn } = req.params;
   console.log(`PUT /books/${isbn}/read`);
-  await Book.updateOne({'isbn': isbn}, {'read': true});
+  await Book.updateOne({ isbn: isbn }, { read: true });
   res.status(201).json();
 });
 
-app.get('/books/unread', (req, res) => {
+app.get("/books/unread", async (req, res) => {
   console.log("GET /books/unread");
 
-  Book.find({'read': false})
-    .sort({'num_pages': -1})
-    .limit(20)
-    .then((results) => {
-      // Succesfull
-      res.json(results);
-    }).catch((err) => {
-      // Error/Failure
-      console.log('Error ' + err);
-      res.json(null);
-    });
+  const unreadBooks = await Book.find({ read: false })
+    .sort({ num_pages: -1 })
+    .limit(20);
+
+  // Should there be an error here?
+  res.status(200).json(unreadBooks);
 });
 
-app.get('/books/:isbn', (req, res) => {
+app.get("/books/:isbn", async (req, res) => {
   const isbn = req.params.isbn;
   console.log(`GET /books/${isbn}`);
-  Book.findOne({'isbn': isbn})
-    .then((results) => {
-      res.json(results);
-    }).catch((err) => {
-      res.json({message: 'Cannot find this book', err: err});
-    });
+  const book = await Book.findOne({ isbn: isbn });
+  if (book) {
+    res.status(200).json(results);
+  } else {
+    res.status(404).json({ message: ERR_CANNOT_FIND_ISBN, err: err });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
-
-
-async function printNumber(num) {
-  console.log(num);
-};
-
-const test = async () => {
-  printNumber(1);
-  printNumber(2);
-  printNumber(3);
-  printNumber(4);
-  printNumber(5);
-  printNumber(6);
-  printNumber(7);
-};
-
-test();
-
+  console.log(`Server running on http://localhost:${port}`);
+});
